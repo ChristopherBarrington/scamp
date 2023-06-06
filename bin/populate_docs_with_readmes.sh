e930339db20fb70f7fcd32f472a79f1995a13ab4
Christopher Barrington <christopher.barrington@crick.ac.uk>
2023-06-06 17:30:55 +0100

    Update populate_docs_with_readmes.sh

--- bin/populate_docs_with_readmes.sh
+++ bin/populate_docs_with_readmes.sh
@@ -5,6 +5,6 @@ find scamp/modules -name 'readme.yaml' |
 	sed -e 's|^scamp/||' -e 's|/readme.yaml$||' |
 	xargs -n 1 -I @ sh -c "SCAMP_MODULE_DOC=scamp/@/readme.yaml hugo new --kind doc-module --force @.md"
 
-find content/modules -mindepth 1 -type d |
+find content/modules -mindepth 0 -type d |
 	xargs -n 1 -I @ sh -c "hugo new --kind docs-group --force @/_index.md"
 )
