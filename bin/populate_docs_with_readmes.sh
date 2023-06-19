8e13e6ab655ea9b3eddc15b969d27be7ec92cfb7
Christopher Barrington <christopher.barrington@crick.ac.uk>
2023-06-19 21:04:09 +0100

    Update populate_docs_with_readmes.sh

--- bin/populate_docs_with_readmes.sh
+++ bin/populate_docs_with_readmes.sh
@@ -4,11 +4,15 @@ cd docs || exit
 
 find scamp/modules -name 'readme.yaml' |
 	sed -e 's|^scamp/||' -e 's|/readme.yaml$||' |
-	xargs -n 1 -I @ sh -c "SCAMP_DOC=scamp/@/readme.yaml hugo new --kind doc-module --force @.md"
+	xargs -n 1 -P 4 -I @ sh -c "SCAMP_DOC=scamp/@/readme.yaml hugo new --kind doc-module --force @.md"
 
 find scamp/workflows -name 'readme.yaml' |
 	sed -e 's|^scamp/||' -e 's|/readme.yaml$||' |
-	xargs -n 1 -I @ sh -c "SCAMP_DOC=scamp/@/readme.yaml hugo new --kind doc-workflow --force @.md"
+	xargs -n 1 -P 4 -I @ sh -c "SCAMP_DOC=scamp/@/readme.yaml hugo new --kind doc-workflow --force @.md"
 
-find content/{modules,workflows} -mindepth 0 -type d |
-	xargs -n 1 -I @ sh -c "hugo new --kind docs-group --force @/_index.md"
+find scamp/utilities -name 'main.yaml' |
+	sed -e 's|^scamp/||' -e 's|/main.yaml$||' |
+	xargs -n 1 -P 4 -I @ sh -c "SCAMP_DOC=scamp/@/main.yaml hugo new --kind doc-utility --force @.md"
+
+find content/{modules,workflows,utilities} -mindepth 0 -type d |
+	xargs -n 1 -P 4 -I @ sh -c "hugo new --kind docs-group --force @/_index.md"
