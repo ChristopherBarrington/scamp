abd266c38f3db1a213919db8d179f1afbe4ede92
Christopher Barrington <christopher.barrington@crick.ac.uk>
2023-10-06 12:13:51 +0100

    update tools cat
    reformat command

--- modules/tools/cat/templates/main.sh
+++ modules/tools/cat/templates/main.sh
@@ -1,3 +1,6 @@
 #! bash
 
-find -L input_* -regextype posix-extended -regex '$regex' -type f | sort --version-sort | xargs cat > $output_file
+find -L input_* -regextype posix-extended -regex '$regex' -type f \\
+| sort --version-sort \\
+| tee catted_files \\
+| xargs cat > $output_file
