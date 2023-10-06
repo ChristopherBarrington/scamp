ad211a933dd0963547d76f55e7695f03224fff20
Christopher Barrington <christopher.barrington@crick.ac.uk>
2023-10-06 14:23:39 +0100

    reformat stub task writers

--- modules/cell_ranger/mkref/templates/stub.sh
+++ modules/cell_ranger/mkref/templates/stub.sh
@@ -9,13 +9,15 @@ touch $assembly/fasta/{genome.fa,genome.fa.fai}
 # write task information to a (yaml) file
 cat <<-END_TASK > task.yaml
 "${task.process}":
-    assembly: $assembly
-    assembly_fasta: `pwd`/assembly.fasta
-    features_gtf: `pwd`/parsed_features.gtf
-    task_index: ${task.index}
-    ext:
-        mkref: ${mkref_args}
-    versions:
-        cell ranger: `cellranger --version | sed 's/cellranger cellranger-//'`
-    work_dir: `pwd`
+    ${task.index}:
+        ext:
+            mkref: ${mkref_args}
+        params:
+            assembly: $assembly
+            assembly_fasta: `pwd`/assembly.fasta
+            features_gtf: `pwd`/parsed_features.gtf
+        task:
+            work_dir: `pwd`
+        versions:
+            cell ranger: `cellranger --version | sed 's/cellranger cellranger-//'`
 END_TASK
--- modules/samtools/faidx/templates/stub.sh
+++ modules/samtools/faidx/templates/stub.sh
@@ -5,10 +5,12 @@ touch versions.yaml ${fasta}.fai
 # write task information to a (yaml) file
 cat <<-END_TASK > task.yaml
 "${task.process}":
-    fasta: $fasta
-    task_index: ${task.index}
-    ext:
-    versions:
-        `samtools help version | head -n 2 | sed 's/Using //' | sed 's/ /: /' | sed '2,\$ s/^/        /'`
-    work_dir: `pwd`
+    ${task.index}:
+        ext:
+        params:
+            fasta: $fasta
+        versions:
+            `samtools help version | head -n 2 | sed 's/Using //' | sed 's/ /: /' | sed '2,\$ s/^/            /'`
+        task:
+            work_dir: `pwd`
 END_TASK
