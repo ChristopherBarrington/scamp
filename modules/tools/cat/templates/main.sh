89ca95b7b9b0db815927b870b277e8c933746bf5
Christopher Barrington <christopher.barrington@crick.ac.uk>
2023-10-06 13:29:53 +0100

    add task writer to cat

--- modules/tools/cat/templates/main.sh
+++ modules/tools/cat/templates/main.sh
@@ -4,3 +4,15 @@ find -L input_* -regextype posix-extended -regex '$regex' -type f \\
 | sort --version-sort \\
 | tee catted_files \\
 | xargs cat > $output_file
+
+# write task information to a (yaml) file
+cat <<-END_TASK > task.yaml
+"${task.process}":
+    regex: $regex
+    files:
+        `sed '2,\$ s/^/        /' catted_files`
+    task_index: ${task.index}
+    ext:
+    versions:
+    work_dir: `pwd`
+END_TASK
