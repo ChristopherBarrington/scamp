0bcf48e0fa2dcb3bcaf7a1c1748be2cc77bfb467
Christopher Barrington <christopher.barrington@crick.ac.uk>
2023-10-09 16:50:57 +0100

    update yaml script for cat

--- modules/tools/cat/templates/yaml.sh
+++ modules/tools/cat/templates/yaml.sh
@@ -1,3 +1,20 @@
 #! bash
 
 yq eval-all '. as \$item ireduce ({}; . * \$item )' input_* > $output_file
+
+# write task information to a (yaml) file
+cat <<-END_TASK > task.yaml
+"${task.process}":
+  task:
+    '${task.index}':
+      params:
+        regex: "$regex"
+        files:
+          - `sed '2,\$ s/^/          - /' catted_files`
+      meta:
+        work_dir: `pwd`
+  process:
+    ext: []
+    versions:
+      yq: `yq --version | sed 's/.*v//'`
+END_TASK
